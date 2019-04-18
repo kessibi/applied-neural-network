a3b1fa23a171718b71bc907604b70dfc0e2c4163
LAJARGE CYRIL <cyril.lajarge@etu.unistra.fr>
2019-04-18 18:11:52 +0200

    main: add parser for arguments

--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python
 
 import numpy as np
+import sys
 #np.set_printoptions(threshold=np.nan)
 from DataLib import DataLib
 from NeuralNet import NeuralNet
 from NNLib import NNLib
+import argparse
 
 def main():
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description='Neural network for heart disease.')
+    parser.add_argument('--batchsize', help='size of batches')
+    parser.add_argument('--nhlayers', help='number of hidden layers')
+    parser.add_argument('--hlayerssizes', nargs='+', help='sizes of hidden layers')
+    args = parser.parse_args()
+    if int(args.nhlayers) != len(args.hlayerssizes):
+        parser.error('hlayerssizes lengths must be equal to nhlayers.')
+    sizes = list(map(int, args.hlayerssizes))
+
+    # Data processing
     data = DataLib.csvToArray("../heart_disease_dataset.csv")
     DataLib.shuffleData(data)
-    
     dataNorm = DataLib.normalizeData(data,np.array([0,3,4,7,9,11]))
+
+    #Neural network
     
     # second argument = batchSize
     # third argument = number of classes
     # fourth argument = number of hidden layers
-    nn = NeuralNet(dataNorm,1,2,3,[5,7,5])
+    nn = NeuralNet(dataNorm,int(args.batchsize),2,int(args.nhlayers),sizes)
 
     nn.train(500)
 
